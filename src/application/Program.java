ee3b0d018c650ddea6105d7da6b9e79d3d6e1540
Victor Tommaz <victortommaz@gmail.com>
2020-07-01 00:26:22 -0300

    first class included

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,16 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-
+		
+		Position pos = new Position();
+		pos.setLinha(3);
+		pos.setColuna(5);
+		
+		System.out.println(pos);
 
 	}
 
